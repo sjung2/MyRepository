32c9c1361ad065bad1a1f870d3b228eaa0e39105
sjung2 <sjung2@oakland.edu>
2024-01-24 21:49:01 -0500

    Assignment4

--- src/Assignment_4.java
+++ src/Assignment_4.java
@@ -4,30 +4,22 @@ public class Assignment_4 {
     public static void  main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Get user input
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a sentence: ");
         String userInput = scanner.nextLine();
 
-        // Call the reverseString method
         String reversedString = reverseString(userInput);
 
-        // Display the reversed string
-        System.out.println("Reversed string: " + reversedString);
+        System.out.println("Reversed sentence: " + reversedString);
     }
-
-    // Method to reverse a string
     private static String reverseString(String str) {
-        // Convert the string to a character array
         char[] charArray = str.toCharArray();
 
-        // Reverse the character array
         for (int i = 0, j = charArray.length - 1; i < j; i++, j--) {
             char temp = charArray[i];
             charArray[i] = charArray[j];
             charArray[j] = temp;
         }
 
-        // Convert the character array back to a string
         return new String(charArray);
     }
 }
